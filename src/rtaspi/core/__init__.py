1e43a245b2476f346be41062475aa217a0227ddc
Tom Softreck <info@softreck.dev>
2025-04-28 21:59:32 +0200

    update

--- src/rtaspi/core/__init__.py
+++ src/rtaspi/core/__init__.py
@@ -3,8 +3,8 @@ Core package initialization.
 """
 
 from .mcp import MCPBroker
-from .config import Config
+from .config import ConfigManager
 from .logging import setup_logging
 from .utils import get_version
 
-__all__ = ['MCPBroker', 'Config', 'setup_logging', 'get_version']
+__all__ = ['MCPBroker', 'ConfigManager', 'setup_logging', 'get_version']
