8954b8971985d68405ed8abf3df68b37ea4223ee
Tom Softreck <info@softreck.dev>
2025-04-28 21:57:55 +0200

    update

--- src/rtaspi/__init__.py
+++ src/rtaspi/__init__.py
@@ -3,5 +3,13 @@ rtaspi package initialization.
 """
 
 from ._version import __version__
+from . import core
+from . import device_managers
+from . import streaming
 
-__all__ = ['__version__']
+__all__ = [
+    '__version__',
+    'core',
+    'device_managers',
+    'streaming'
+]
